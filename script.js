feb40b72b6a193f6d03936a221b841dc60564616
abhayy947 <71834710+abhayy947@users.noreply.github.com>
2020-12-18 13:48:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
+// var botton=document.getElementById("Button");
+// var input=document.getElementById("input");
+// var ul=document.querySelector("ul");
+
+// function inputlength(){
+// 	return input.value.length;
+// }
+
+// function createListItem(){
+//     var li=document.createElement("li"); 
+//     li.appendChild(document.createTextNode(input.value));
+// 	ul.appendChild(li);
+// 	input.value="";
+// }
+// function addListAfterClick(){
+// 	if(inputlength()>0)
+// 	createListItem();
+// }
+//  function addListAfterKeyPressed(event){
+// 	if(inputlength()>0 && event.keyCode===13)
+// 	createListItem();
+// }
+
+// button.addEventListener("click",addListAfterClick);
+
+// input.addEventListener("keypress",addListAfterKeyPressed);
+
+
+
 
 
  var h3=document.querySelector("h3");
 var color1=document.querySelector(".color1");
 var color2=document.querySelector(".color2");
 var gradient=document.getElementById("gradient");
-  color1.addEventListener("input",function(){
-  	gradient.style.background="linear-gradient( to right,"
-  	+color1.value
-  	+","
-  	+color2.value+")";
-  	h3.textContent=gradient.style.background+";";
-  })
-  color2.addEventListener("input",function(){
+function createGradient(){
   	gradient.style.background="linear-gradient( to right,"
   	+color1.value
   	+","
   	+color2.value+")";
     h3.textContent=gradient.style.background+";";
-  })
-console.log(h3);
+  }
+  color1.addEventListener("input",createGradient)
+
+  color2.addEventListener("input",createGradient)
+
+
  
 
 
