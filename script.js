07379f5e8f7d9ee688eea5551766038c03943f93
abhayy947 <71834710+abhayy947@users.noreply.github.com>
2020-12-18 13:49:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,4 @@
-// var botton=document.getElementById("Button");
-// var input=document.getElementById("input");
-// var ul=document.querySelector("ul");
-
-// function inputlength(){
-// 	return input.value.length;
-// }
-
-// function createListItem(){
-//     var li=document.createElement("li"); 
-//     li.appendChild(document.createTextNode(input.value));
-// 	ul.appendChild(li);
-// 	input.value="";
-// }
-// function addListAfterClick(){
-// 	if(inputlength()>0)
-// 	createListItem();
-// }
-//  function addListAfterKeyPressed(event){
-// 	if(inputlength()>0 && event.keyCode===13)
-// 	createListItem();
-// }
-
-// button.addEventListener("click",addListAfterClick);
-
-// input.addEventListener("keypress",addListAfterKeyPressed);
-
-
-
-
-
- var h3=document.querySelector("h3");
+var h3=document.querySelector("h3");
 var color1=document.querySelector(".color1");
 var color2=document.querySelector(".color2");
 var gradient=document.getElementById("gradient");
